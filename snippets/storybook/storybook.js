e458dcc1b66e106a8dece3765f41b072364969df
Brandon Yee <brandon.yee@socotra.com>
2019-03-23 15:06:20 -0700

    updated storybook snippet

--- snippets/storybook/storybook.js
+++ snippets/storybook/storybook.js
@@ -2,10 +2,12 @@
 ==== Regular Dependencies (use 'npm i'): ====
 None
 
-// Initialize storybook in your project dir.
-// Creates 'stories' folder at top level but can be moved elsewhere. Basically will pick up any files ending in *.stories.js).
-// Also creates .storybook folder which contain config files for the storybook app
+// Initialize storybook in your project
 npx -p @storybook/cli sb init
+
+* Creates 'stories' folder at top level but can be moved elsewhere. Basically will pick up any files ending in *.stories.js).
+* Creates .storybook folder which contain config files for the storybook app
+* Modifies your package.json to add storybook dependencies and scripts
 
 // Run the storybook app. Runs at: localhost:6006
 npm run storybook
