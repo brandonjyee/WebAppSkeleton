f8d25c9de187e1160adfb0cd5b894cfabf2d95f5
Brandon Yee <brandon.yee@socotra.com>
2019-03-23 15:01:53 -0700

    added snippet for storybook

---
+++ snippets/storybook/storybook.js
@@ -0,0 +1,12 @@
+/*
+==== Regular Dependencies (use 'npm i'): ====
+None
+
+// Initialize storybook in your project dir.
+// Creates 'stories' folder at top level but can be moved elsewhere. Basically will pick up any files ending in *.stories.js).
+// Also creates .storybook folder which contain config files for the storybook app
+npx -p @storybook/cli sb init
+
+// Run the storybook app. Runs at: localhost:6006
+npm run storybook
+*/
